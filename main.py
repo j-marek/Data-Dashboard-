7a44b15d43608b4483072c230fbd98cbf9c35f9f
j-marek <91067069+j-marek@users.noreply.github.com>
2022-08-21 11:05:16 +0000

    added audio element

--- main.py
+++ main.py
@@ -10,12 +10,20 @@ data = {
 df = pandas.DataFrame(data)
 
 st.title('I made this website')
-st.subheader("It ain't much, but it's honest work")
+st.subheader("It ain't much, but it's honest work.")
 st.write('''
              Here is some data in a table with no real meaning.
 ''')
 st.write(df)
+st.write('''
+             Here is that data in a line chart!
+''')
 st.line_chart(df)
 
 myslider = st.slider('lbs')
 st.write(myslider, 'lbs in kgs is', myslider * 0.453592)
+
+st.write('''
+             And some audio to top it off!
+''')
+st.audio("https://upload.wikimedia.org/wikipedia/commons/c/c5/De-Otter.ogg")
